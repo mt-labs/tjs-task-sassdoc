a97ce1cfb20f40800c98ee715fdf697aa7372cb5
Matthew Lawrence-Stubley <quakematt@outlook.com>
2017-02-25 20:44:32 +0000

    Create a simple (manual) test case

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+'use strict';
+
+var gelf = require('tjs-gelf');
+
+
+/**
+ * Task: Default.
+ */
+gelf.task('default', ['dump:tasks']);
+
+
+/**
+ * Task module: Simple test.
+ */
+gelf.load(require('./index'), function(config) {
+
+	config.docSrc = 'src/**/*.scss';
+	
+	config.patternSrc = [
+		'src/patterns.scss',
+		'src/**/patterns.scss',
+	];
+	
+	config.dest = 'dist/sassdoc';
+
+});
