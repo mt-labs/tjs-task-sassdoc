c40e2408e6a393c206d6a3e0396ecad2ec8df9f4
Matthew Lawrence-Stubley <quakematt@outlook.com>
2017-02-25 20:45:36 +0000

    Trim excess whitespace

--- gulpfile.js
+++ gulpfile.js
@@ -15,12 +15,12 @@ gelf.task('default', ['dump:tasks']);
 gelf.load(require('./index'), function(config) {
 
 	config.docSrc = 'src/**/*.scss';
-	
+
 	config.patternSrc = [
 		'src/patterns.scss',
 		'src/**/patterns.scss',
 	];
-	
+
 	config.dest = 'dist/sassdoc';
 
 });
